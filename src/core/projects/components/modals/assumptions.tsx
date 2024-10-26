99ed97b10d8069094ddaf621650f4c74c3415dfd
owaisjalal-10p <owais.jalal@10pearls.com>
2024-10-26 14:59:52 +0500

    Table added

--- src/core/projects/components/modals/assumptions.tsx
+++ src/core/projects/components/modals/assumptions.tsx
@@ -3,86 +3,80 @@ import { Dialog } from "primereact/dialog";
 import { InputTextarea } from "primereact/inputtextarea";
 import { RadioButton } from "primereact/radiobutton";
 
-interface IProps{
-    confirmationProjectName: string,
-    confirmationVisible: boolean,
-    setConfirmationVisible: (isVisible: boolean) => void,
-    assumtions: Array<any>,
-    handleSaveResponse: (assumtions: Array<any>) => void,
-    setAssumtions: (assumtions: any) => void
+interface IProps {
+  confirmationProjectName: string;
+  confirmationVisible: boolean;
+  setConfirmationVisible: (isVisible: boolean) => void;
+  assumtions: Array<any>;
+  handleSaveResponse: (assumtions: Array<any>) => void;
+  setAssumtions: (assumtions: any) => void;
 }
 
-function Assumption(props: IProps){
-    const {confirmationProjectName, confirmationVisible, setConfirmationVisible, assumtions, handleSaveResponse, setAssumtions} = {...props};
-    const updateAssumptionState = () => {
-        setAssumtions(() => [...assumtions])
-    }
-    
-    return (
-        <Dialog
-        header={confirmationProjectName}
-        visible={confirmationVisible}
-        style={{ width: "60vw" }}
-        onHide={() => setConfirmationVisible(false)}
-      >
-        <div className="col-12">
-          <div className="card">
-            <div className="col-12 mb-2 h-20rem overflow-auto">
-              {assumtions.map((assumption, index) => (
-                <div
-                  key={index}
-                  className="flex justify-content-between mb-2 grid"
-                >
-                  <div className="col-10">
-                    <p
-                      style={{
-                        textDecoration: assumption.isRemovedFromList
-                          ? "line-through"
-                          : "none",
-                        }}
-                    >
-                      {assumption.assumption}
-                    </p>
-                  </div>
-                  <div className="actions col-2 justify-content-end flex gap-2">
-                    <div className="flex align-items-center">
-                        <RadioButton inputId="assumption1" name="assumtion" value="Yes" 
-                            onChange={(e) => {
-                                assumption.value = true;
-                                updateAssumptionState();
-                            }} 
-                            checked={assumption.value} />
-                        <label htmlFor="assumption1" className="ml-2">YES</label>
-                    </div>
-                    <div className="flex align-items-center">
-                        <RadioButton inputId="assumption2" name="assumtion" value="No" 
-                            onChange={(e) => {
-                                assumption.value = false;
-                                updateAssumptionState();
-                            }} 
-                            checked={!assumption.value} />
-                        <label htmlFor="assumption2" className="ml-2">NO</label>
-                    </div>
-                  </div>
+function Assumption(props: IProps) {
+  const {
+    confirmationProjectName,
+    confirmationVisible,
+    setConfirmationVisible,
+    assumtions,
+    handleSaveResponse,
+    setAssumtions,
+  } = { ...props };
+  const updateAssumptionState = () => {
+    setAssumtions(() => [...assumtions]);
+  };
+
+  return (
+    <Dialog
+      header={confirmationProjectName}
+      visible={confirmationVisible}
+      style={{ width: "60vw" }}
+      onHide={() => setConfirmationVisible(false)}
+    >
+      <div className="col-12">
+        <div className="card">
+          <div className="col-12 mb-2 h-20rem overflow-auto">
+            {assumtions.map((assumption, index) => (
+              <div
+                key={index}
+                className="flex justify-content-between mb-2 grid"
+              >
+                <div className="col-10">
+                  <p
+                    style={{
+                      textDecoration: assumption.isRemovedFromList
+                        ? "line-through"
+                        : "none",
+                    }}
+                  >
+                    {assumption.assumption}
+                  </p>
                 </div>
-              ))}
-            </div>
+                <div className="actions col-2 justify-content-end flex gap-2">
+                  <Button icon="pi pi-check" rounded text aria-label="Filter" />
+                  <Button
+                    icon="pi pi-times"
+                    rounded
+                    text
+                    severity="danger"
+                    aria-label="Cancel"
+                  />
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-        <div className="col-12 flex justify-content-end">
-          <Button
-            label="Save"
-            onClick={() => handleSaveResponse(assumtions)}
-          />
-          <Button
-            outlined
-            label="Cancel"
-            onClick={() => setConfirmationVisible(false)}
-            className="ml-2"
-          />
-        </div>
-      </Dialog>
-    );
+      </div>
+      <div className="col-12 flex justify-content-end">
+        <Button label="Save" onClick={() => handleSaveResponse(assumtions)} />
+        <Button
+          outlined
+          label="Cancel"
+          onClick={() => setConfirmationVisible(false)}
+          className="ml-2"
+        />
+      </div>
+    </Dialog>
+  );
 }
 
 export default Assumption;
--- src/core/projects/components/modals/estimations.tsx
+++ src/core/projects/components/modals/estimations.tsx
@@ -1,70 +1,57 @@
 import { Dialog } from "primereact/dialog";
+import React, { useState, useEffect } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 import { Button } from "primereact/button";
-import { RadioButton } from "primereact/radiobutton";
 
-interface IProjectDetails{
-    confirmationProjectName: string,
-    confirmationVisible: boolean,
-    setConfirmationVisible: (isVisible: boolean) => void,
-    assumtions: Array<any>,
-    handleSaveResponse: (assumtions: Array<any>) => void,
-    setAssumtions: (assumtions: any) => void
+interface IProjectDetails {
+  confirmationProjectName: string;
+  confirmationVisible: boolean;
+  setConfirmationVisible: (isVisible: boolean) => void;
+  assumtions: Array<any>;
+  handleSaveResponse: (assumtions: Array<any>) => void;
+  setAssumtions: (assumtions: any) => void;
 }
 
-function EstimationsListModal(props: IProjectDetails){
-    const {confirmationProjectName, confirmationVisible, setConfirmationVisible, assumtions, handleSaveResponse, setAssumtions} = {...props};
-    const updateAssumptionState = () => {
-        setAssumtions(() => [...assumtions])
-    }
-    
-    return (
-        <Dialog
-        header={confirmationProjectName}
-        visible={confirmationVisible}
-        style={{ width: "60vw" }}
-        onHide={() => setConfirmationVisible(false)}
-      >
-        <div className="col-12">
-          <div className="card">
-            <div className="col-12 mb-2 h-20rem overflow-auto">
-              {assumtions.map((assumption, index) => (
-                <div
-                  key={index}
-                  className="flex justify-content-between mb-2 grid"
-                >
-                  <div className="col-10">
-                    <p
-                      style={{
-                        textDecoration: assumption.isRemovedFromList
-                          ? "line-through"
-                          : "none",
-                        }}
-                    >
-                      {assumption.assumption}
-                    </p>
-                  </div>
-                  <div className="actions col-2 justify-content-end flex gap-2">
-                    
-                  </div>
-                </div>
-              ))}
-            </div>
+function EstimationsListModal(props: IProjectDetails) {
+  const {
+    confirmationProjectName,
+    confirmationVisible,
+    setConfirmationVisible,
+    assumtions,
+    handleSaveResponse,
+  } = { ...props };
+
+  return (
+    <Dialog
+      header={confirmationProjectName}
+      visible={confirmationVisible}
+      style={{ width: "60vw" }}
+      onHide={() => setConfirmationVisible(false)}
+    >
+      <div className="col-12">
+        <div className="card">
+          <div className="col-12 mb-2 h-20rem overflow-auto">
+            {/* <DataTable value={products} tableStyle={{ minWidth: "50rem" }}>
+              <Column field="code" header="Code"></Column>
+              <Column field="name" header="Name"></Column>
+              <Column field="category" header="Category"></Column>
+              <Column field="quantity" header="Quantity"></Column>
+            </DataTable> */}
           </div>
         </div>
-        <div className="col-12 flex justify-content-end">
-          <Button
-            label="Save"
-            onClick={() => handleSaveResponse(assumtions)}
-          />
-          <Button
-            outlined
-            label="Cancel"
-            onClick={() => setConfirmationVisible(false)}
-            className="ml-2"
-          />
-        </div>
-      </Dialog>
-    );
+      </div>
+      <div className="col-12 flex justify-content-end">
+        <Button label="Save" onClick={() => handleSaveResponse(assumtions)} />
+        <Button
+          outlined
+          label="Cancel"
+          onClick={() => setConfirmationVisible(false)}
+          className="ml-2"
+        />
+      </div>
+    </Dialog>
+  );
 }
 
 export default EstimationsListModal;
